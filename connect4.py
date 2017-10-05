f9c8dc81d364e0e086789c30483e3273b15d5d3d
vaporwave123 <raghulrajasekar@yahoo.co.in>
2017-10-05 22:24:50 +0530

    Added alpha-beta pruning

--- connect4.py
+++ connect4.py
@@ -3,6 +3,9 @@ import copy
         
 root = tk.Tk()
 
+INF = 10000000000000
+DEPTH = 5
+
 c = tk.Canvas(root, height=1000, width=1000, bg='white')
 c.pack()
 rect = []
@@ -37,7 +40,6 @@ def heuristic(state, tag, curr, currcoins, ccol, currheur):
     block = []
     for i in range(4):
         block.append(state[tag][start+i])
-    #print('ver',block)
     ret-=eval4(block)
     block.remove(0)
     block.append(ccol)
@@ -47,7 +49,6 @@ def heuristic(state, tag, curr, currcoins, ccol, currheur):
     for i in range(end-start-3):
         block.remove(state[tag][start+i])
         block.append(state[tag][start+4+i])
-        #print(block)
         ret-=eval4(block)
         block.remove(0)
         block.append(ccol)
@@ -60,7 +61,6 @@ def heuristic(state, tag, curr, currcoins, ccol, currheur):
     block = []
     for i in range(4):
         block.append(state[start+i][curr[tag]])
-    #print('hor',block)
     ret-=eval4(block)
     block.remove(0)
     block.append(ccol)
@@ -70,7 +70,6 @@ def heuristic(state, tag, curr, currcoins, ccol, currheur):
     for i in range(end-start-3):
         block.remove(state[start+i][curr[tag]])
         block.append(state[start+4+i][curr[tag]])
-        #print(block)
         ret-=eval4(block)
         block.remove(0)
         block.append(ccol)
@@ -88,7 +87,6 @@ def heuristic(state, tag, curr, currcoins, ccol, currheur):
         block = []
         for i in range(4):
             block.append(state[tag-start+i][curr[tag]+start-i])
-        #print('diag1',block)
         ret-=eval4(block)
         block.remove(0)
         block.append(ccol)
@@ -98,7 +96,6 @@ def heuristic(state, tag, curr, currcoins, ccol, currheur):
         for i in range(end+start-3):
             block.remove(state[tag-start+i][curr[tag]+start-i])
             block.append(state[tag-start+4+i][curr[tag]+start-4-i])
-            #print(block)
             ret-=eval4(block)
             block.remove(0)
             block.append(ccol)
@@ -116,7 +113,6 @@ def heuristic(state, tag, curr, currcoins, ccol, currheur):
         block = []
         for i in range(4):
             block.append(state[tag-start+i][curr[tag]-start+i])
-        #print('diag2',block)
         ret-=eval4(block)
         block.remove(0)
         block.append(ccol)
@@ -126,7 +122,6 @@ def heuristic(state, tag, curr, currcoins, ccol, currheur):
         for i in range(end+start-3):
             block.remove(state[tag-start+i][curr[tag]-start+i])
             block.append(state[tag-start+4+i][curr[tag]-start+4+i])
-            #print(block)
             ret-=eval4(block)
             block.remove(0)
             block.append(ccol)
@@ -157,25 +152,25 @@ def validateState(state, tag, curr, currcoins, ccol):
     else:
         return -1 
         
-def negamax(state, tag, curr, ccoins, ccol, currheur, depth):
+def negamax(state, tag, curr, ccoins, ccol, currheur, depth, alpha, beta):
     newcheur = heuristic(state, tag, curr, ccoins, ccol, currheur)
     if(depth==0 or abs(newcheur)>1000000000 or ccoins==41):
         if(ccol==2):
             newcheur*=-1
-        #print(tag,ccoins,depth,newcheur,state)
         return newcheur
-    bestval = []
     state[tag][curr[tag]]=ccol
     curr[tag]+=1
     for i in range(7):
         if(curr[i]<6):
-            bestval.append(-negamax(copy.deepcopy(state), i, curr[:], ccoins+1, 3-ccol, newcheur, depth-1))
-    #print(ccol,tag,ccoins,depth,bestval,state)
-    return min(bestval)    
+            ret = -negamax(copy.deepcopy(state), i, curr[:], ccoins+1, 3-ccol, newcheur, depth-1, -beta, -alpha)
+            beta = min(beta,ret)
+            if(alpha>beta):
+                break
+    return beta    
 
 def updatescoreTxt(score):
     for i in range(7):
-        if(score[i]!=-10000000000000):
+        if(score[i]!=-INF):
             c.itemconfig(scoreTxt[i], text="%d"%score[i])
         else:
             c.itemconfig(scoreTxt[i], text="Full")
@@ -212,37 +207,26 @@ def onCircleClick(event,tag):
         nextlist = []
         for i in range(7):
             if(curr[i]<6):
-                nextlist.append(negamax(copy.deepcopy(currstate),i,curr[:],currcoins,currcol,cheur,4))
+                nextlist.append(negamax(copy.deepcopy(currstate),i,curr[:],currcoins,currcol,cheur,DEPTH-1,-INF,INF))
             else:
-                nextlist.append(-10000000000000)
+                nextlist.append(-INF)
         updatescoreTxt(nextlist)
-        #playnext = nextlist.index(max(nextlist))
-        #print(currstate)
-        """nextlist = []
-        for i in range(7):
-            if(curr[i]<5):
-                nextlist.append(heuristic(currstate,i,curr,currcoins,currcol,cheur))
-            else:
-                nextlist.append(12345678912345)
-        print(nextlist)"""
         if(ret==-1):
-            #print("Player %d's turn"%currcol)
             c.itemconfig(txt, text="Player %d's turn"%currcol)
         elif(ret==0):
-            #print("It's a draw!")
             c.itemconfig(txt, text="  It's a draw!")
             unbindAll()
             root.after(2000,root.destroy)
             return
         else:
-            #print("Player %d wins!"%ret)
             c.itemconfig(txt, text="Player %d wins!"%ret)
             unbindAll()
             root.after(2000,root.destroy)
             return
         if(curr[tag]<6):
             onCircleEnter(event,tag)
-        """if(currcol==2):
+        """playnext = nextlist.index(max(nextlist))
+        if(currcol==2):
             onCircleClick(event,playnext)
             onCircleEnter(event,tag)"""
     
@@ -259,6 +243,6 @@ for i in range(7):
     c.tag_bind(rect[i], '<Leave>', lambda event, tag=i: onCircleLeave(event, tag))
     c.tag_bind(rect[i], '<1>', lambda event, tag=i: onCircleClick(event, tag))
 txt = c.create_text(350, 800, text="Player 1's turn", font=('Purisa',40), anchor="sw")
-scoreTxt = [(c.create_text(150+100*i, 80, text="0", anchor="sw")) for i in range(7)]
+scoreTxt = [(c.create_text(170+100*i, 80, text="0", anchor="sw")) for i in range(7)]
 
 root.mainloop()
